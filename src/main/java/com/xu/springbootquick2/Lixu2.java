c18a1414f3c75a8c0e68c1def59f98560c326fb3
admin <332424882@qq.com>
2019-07-29 15:14:26 +0800

    springbootquick第一次提交

---
+++ src/main/java/com/xu/springbootquick2/Lixu2.java
@@ -0,0 +1,5 @@
+package com.xu.springbootquick2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
---
+++ src/main/java/com/xu/springbootquick2/SpringbootQuick2Application.java
@@ -0,0 +1,12 @@
+package com.xu.springbootquick2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuick2Application {
+
+
+
+
+}
